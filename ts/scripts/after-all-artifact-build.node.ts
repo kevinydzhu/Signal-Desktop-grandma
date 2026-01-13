bf721cd7caf4484f59642467ef6276d860a2e4f7
Kevin Zhu <kevin.yd.zhu@gmail.com>
2026-01-13 11:07:24 +1100

    feat: remove all unnecessary workflows + code signing

--- ts/scripts/after-all-artifact-build.node.ts
+++ ts/scripts/after-all-artifact-build.node.ts
@@ -2,11 +2,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import type { BuildResult } from 'electron-builder';
-import { afterAllArtifactBuild as notarizeUniversalDMG } from './notarize-universal-dmg.node.js';
 
 export async function afterAllArtifactBuild(
-  result: BuildResult
+  _result: BuildResult
 ): Promise<Array<string>> {
-  await notarizeUniversalDMG(result);
   return [];
 }
